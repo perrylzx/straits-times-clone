69734b932fea49a7ca75af94e11619cf6df0eb64
perrylzx <perrylzx@gmail.com>
2020-02-03 17:54:00 +0800

    incorporated react-bootstrap, incorporated barebones layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar1 from './Components/Navbar'
+import StoryCard from './Components/Stories';
+import Navbar from 'react-bootstrap/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Straits_times_clone
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3 className='container0'>
+        <div className='header'>
+          <h1>THE STRAITS TIME</h1>
+        </div>
+
+        <div className='iconsHeader'>
+          <h3 className='test'>placeholder for icons</h3>
+        </div>
+      </h3>
+
+      <div className='Navbar'>
+        <div className='navButtons'>
+          <h3>ST</h3>
+        </div>
+        <div className='navButtons'>
+          <h3>SINGAPORE</h3>
+        </div>
+        <div className='navButtons'>
+          <h3>POLITICS</h3>
+        </div>
+        <div className='navButtons'>
+          <h3>ASIA</h3>
+        </div>
+        <div className='navButtons'>
+          <h3>WORLD</h3>
+        </div>
+        <div className='navButtons'>
+          <h3>VIDEOS</h3>
+        </div>
+        <div className='navButtons'>
+          <h3>LIFESTYLE</h3>
+        </div>
+        <div className='navButtons'>
+          <h3>FOOD</h3>
+        </div>
+        <div className='navButtons'>
+          <h3>MORE</h3>
+        </div>
+        <div className='navButtons'>
+          <h3>SEARCHBUTTON</h3>
+        </div>
+      </div>
+
+      <div className='topStories'>
+        <h1>TOP STORIES</h1>
+        <hr></hr>
+      </div>
+
+      <div className='container1'>
+        <div>
+          <StoryCard title='asdsdasds' image='cat.png'></StoryCard>
+        </div>
+        <div>
+          <StoryCard></StoryCard>
+        </div>
+        <div>
+          <StoryCard></StoryCard>
+        </div>
+        <div>
+          <StoryCard></StoryCard>
+        </div>
+        <div>
+          <StoryCard></StoryCard>
+        </div>
+        <div>
+          <StoryCard></StoryCard>
+        </div>
+        <div>
+          <StoryCard></StoryCard>
+        </div>
+        <div>
+          <StoryCard></StoryCard>
+        </div>
+        <div>
+          <StoryCard></StoryCard>
+        </div>
+      </div>
+
     </div>
   );
 }

75d3ad68532b96c7edbd3924cb5f7710d1940ce0
perrylzx <46482518+perrylzx@users.noreply.github.com>
2020-02-07 12:46:23 +0800

    organized imports to alphabetical order

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import $ from 'jquery';
-import Popper from 'popper.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 
